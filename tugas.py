cf2872c5ec9fd9ed4cd69d287c0dd6ec45778b4b
Anisa Muziya Rafa <amrxll08@gmail.com>
2025-03-10 14:48:31 +0800

    tugas.py

---
+++ tugas.py
@@ -0,0 +1,12 @@
+class Kelas_Aritmatika_Dasar():
+    def penjumlahan(self,a,b):
+        return a + b
+class
+    def pengurangan(self,c,d):
+        return c - d
+    
+    penjumlahan_a = penjumlahan()
+    pengurangan_a = pengurangan()
+
+    print(penjumlahan_a.penjumlahan(4,6))
+    print(pengurangan_a.pengurangan(4,6)
